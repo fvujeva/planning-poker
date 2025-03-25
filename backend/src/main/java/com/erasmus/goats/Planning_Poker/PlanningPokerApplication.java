dcb58ccf3b90ed4fe4564d87e04c299fc00f2e6c
Filip Vujeva <filip.vujeva123@gmail.com>
2025-03-25 12:27:59 +0100

    initial backend commit

---
+++ backend/src/main/java/com/erasmus/goats/Planning_Poker/PlanningPokerApplication.java
@@ -0,0 +1,13 @@
+package com.erasmus.goats.Planning_Poker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningPokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningPokerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/erasmus/goats/Planning_Poker/PlanningPokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erasmus.goats.Planning_Poker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningPokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
